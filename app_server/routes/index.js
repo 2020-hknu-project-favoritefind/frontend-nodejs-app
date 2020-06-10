0e618cfb59b774fd26f026a305eeedaf5073d997
vallisneria <moogi3469@gmail.com>
2020-06-10 19:39:28 +0900

    회원가입 구현

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -8,13 +8,14 @@ router.get('/', ctrlHobby.home); //시작화면
 router.get('/test', ctrlHobby.test); //취미분석
 router.get('/find', ctrlHobby.find); //장소 추천-사용자에게 장소/분야 물어보기
 router.get('/map', ctrlHobby.map) //장소 추천2-지도랑 보여주기
-router.get('/place',ctrlHobby.place) //카테고리에서 클릭
+router.get('/place', ctrlHobby.place) //카테고리에서 클릭
 router.get('/place/add', ctrlHobby.add); //장소 추가
 router.get('/place/best', ctrlHobby.best); //인기 장소
 
 /* GET User pages */
 router.get('/login', ctrlUser.login); //로그인
 router.get('/join', ctrlUser.join); //회원가입
+router.post('/join/process', ctrlUser.join_process);//회원가입 과정
 router.get('/like', ctrlUser.like); //즐겨찾기
 
 /* 아직 디자인 안한 페이지 */
